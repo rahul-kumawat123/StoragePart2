d5440b177b4e2ab7b466d2f4cfe3a99566a462ef
Lenovo <rahul.kumawat@tothenew.com>
2021-03-11 23:47:57 +0530

    Intial Commit

--- app/src/main/java/com/example/storagepart2/AppRoomDatabase.kt
+++ app/src/main/java/com/example/storagepart2/AppRoomDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [Employee::class],version = 4)
+@Database(entities = [Employee::class],version = 8)
 abstract class AppRoomDatabase: RoomDatabase() {
     abstract fun employeeDao(): EmployeeDao
 }
